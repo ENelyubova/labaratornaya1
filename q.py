81db0bdac4d40f6c755b677764bacec5ca91b93b
Елена <nelyubova_lena@mail.ru>
2016-12-22 17:51:47 +0500

    5 задание

--- q.py
+++ q.py
@@ -20,12 +20,12 @@ def fib(n):
 print(fib(15))
 
 
-# def Contains(s: str):
-# L = ["й", "ц", "к", "н", "г", "ш", "щ", "з", "х", "ф", "в", "п", "р", "л", "д", "ж", "ч", "с", "м", "т"]
-# for x in s:
-# if x in l:
-# return True
-# return False
+def Contains(s: str):
+l = ["й", "ц", "к", "н", "г", "ш", "щ", "з", "х", "ф", "в", "п", "р", "л", "д", "ж", "ч", "с", "м", "т"]
+for x in s:
+if x in l:
+return True
+return False
 
 def Split(s: str) -> list:
 l = []
@@ -40,5 +40,35 @@ l.append(ss)
 for x in l:
 if Contains(x):
 print(x)
+
+def CreateDictionary(l: list) -> dict:
+d = dict()
+for x in l:
+d[x] = l.count(x)
+return d
+
+def Sq(x) -> bool:
+from math import sqrt
+for i in range(1, int(sqrt(x)+1)):
+if x == i*i:
+return True
+return False
+
+def CreateSets(l: list):
+event = set()
+sq = set()
+for x in l:
+if x % 2 == 0:
+event.add(x)
+for x in l:
+if Sq(x):
+sq.add(x)
+print(event)
+print(sq)
+print("Intersection: ", event.intersection(sq))
+print("Union: ", event.union(sq))
+print("Difference: ", event.difference(sq))
+print("Symmetric Difference: ", event.symmetric_difference(sq))
+
 
 
